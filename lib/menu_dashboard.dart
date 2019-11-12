d1cc89593659e94d6c0a8bc188d2a02a2b9c702a
семён дьяченко <cemchic05@outlook.com>
2019-11-12 07:35:14 +0300

    horisontal scroll

--- lib/menu_dashboard.dart
+++ lib/menu_dashboard.dart
@@ -88,26 +88,27 @@ class MenuDashBoardpage extends StatefulWidget {
                     Icon(Icons.settings, color: Colors.white),
                   ],
                 ),
+                SizedBox(height: 50),
                 Container(
-                  height: 200,
+                  height: 400,
                   child: PageView(
                     controller: PageController(viewportFraction: 0.8),
-                    scrollDirection: Axis.horizontal,
+                    scrollDirection: Axis.vertical,
                     pageSnapping: true,
                     children: <Widget>
                     [
                       Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        color:Colors.redAccent,
+                        width:100,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 8),
                         color:Colors.blueAccent,
                         width:100,
                       ),
                       Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
-                        color:Colors.blueAccent,
-                        width:100,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 8),
+                        margin: const EdgeInsets.symmetric(vertical: 8),
                         color:Colors.greenAccent,
                         width:100,
                       ),
